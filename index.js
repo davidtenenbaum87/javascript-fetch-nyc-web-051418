aca2e7ac64afc8d21643e2970d42c76881ea9d84
David Tenenbaum <github email address>
2018-07-02 19:18:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+// fetch('https://api.github.com/davidtenenbaum87/repos').
+//   then(res => res.json()).
+//   then(json => console.log(json))
+
+const token = '77068a6bad180d5e656ea90cf20579425c3d0a9f'
+fetch('https://api.github.com/davidtenenbaum87/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
